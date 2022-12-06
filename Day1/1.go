b951aaf89baadd91623037df8e9c37f32b673d2d
Victor <victorruicer@hotmail.com>
2022-12-06 20:03:14 +0100

    Day1 - 1 more clear

--- Day1/1.go
+++ Day1/1.go
@@ -18,14 +18,14 @@ func main() {
 	slice := strings.TrimRight(strings.ReplaceAll(string(input), "\r", ""), "\n") //ToArray
 
 	var mostKcal int
-	for _, elfFood := range strings.Split(slice, "\n\n") { // cada elfo esta separado por dos saltos de linea
+	for _, elfFood := range strings.Split(slice, "\n\n") {
 		var totalElfKcal int
 		for _, foodItem := range strings.Split(elfFood, "\n") {
 			if kcal, err := strconv.Atoi(foodItem); err == nil {
 				totalElfKcal += kcal
 			} else {
 				fmt.Println()
-				panic(err) // esto en una app de verdad es prohibido
+				panic(err) // just for tests
 			}
 		}
 		if totalElfKcal > mostKcal {
